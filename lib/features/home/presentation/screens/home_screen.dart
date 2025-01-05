29c9d376097c85c7c38926e91b46b88e7098e483
Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2025-01-06 01:33:41 +0700

    dict ✨ implement GetDailyWord state manager

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/constants/sizing_constants.dart';
 import '../../../../core/services/services_mixin.dart';
+import '../../../dictionary/domain/entities/dictionary_state/dictionary_state.dart';
+import '../../../dictionary/presentation/state_managers/daily_word/daily_word_cubit.dart';
+import '../../../dictionary/presentation/state_managers/dictionary_cubit.dart';
 import '../widgets/home_main_view.dart';
 import '../widgets/home_side_bar.dart';
 import '../widgets/home_state_manager_provider.dart';
@@ -15,22 +19,29 @@ class HomeScreen extends StatelessWidget with ServicesMixin {
   @override
   Widget build(BuildContext context) {
     return HomeStateManagerProvider(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(SizingConstants.homeBentoPadding),
-          child: Row(
-            children: [
-              /* Main View */
-              const Expanded(child: HomeMainView()),
-              const SizedBox(width: SizingConstants.homeBentoPadding),
+      child: BlocListener<DictionaryCubit, DictionaryState>(
+        listenWhen: (previous, current) =>
+            previous.focusedDate != current.focusedDate,
+        listener: (context, state) => context.read<DailyWordCubit>().getData(
+              state.focusedDate,
+            ),
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(SizingConstants.homeBentoPadding),
+            child: Row(
+              children: [
+                /* Main View */
+                const Expanded(child: HomeMainView()),
+                const SizedBox(width: SizingConstants.homeBentoPadding),
 
-              /* Side Bar */
-              SizedBox(
-                width: 296,
-                height: 1.sh,
-                child: const HomeSideBar(),
-              )
-            ],
+                /* Side Bar */
+                SizedBox(
+                  width: 296,
+                  height: 1.sh,
+                  child: const HomeSideBar(),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/home/presentation/widgets/home_state_manager_provider.dart
+++ lib/features/home/presentation/widgets/home_state_manager_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../dictionary/presentation/state_managers/daily_word/daily_word_cubit.dart';
 import '../../../dictionary/presentation/state_managers/dictionary_cubit.dart';
 
 class HomeStateManagerProvider extends StatelessWidget {
@@ -15,8 +16,9 @@ class HomeStateManagerProvider extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider(create: (context) => DictionaryCubit()),
         BlocProvider(
-          create: (context) => DictionaryCubit(),
+          create: (context) => DailyWordCubit()..getData(DateTime.now()),
         ),
       ],
       child: child,
